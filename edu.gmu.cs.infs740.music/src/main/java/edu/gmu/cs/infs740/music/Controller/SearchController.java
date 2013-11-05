d582726ceda87a4df9afab1967d7fabbffd5bb52
Wade Ward <wade.w.ward@gmail.com>
2013-11-05 01:12:46 -0500

    sync

--- edu.gmu.cs.infs740.music/src/main/java/edu/gmu/cs/infs740/music/Controller/SearchController.java
+++ edu.gmu.cs.infs740.music/src/main/java/edu/gmu/cs/infs740/music/Controller/SearchController.java
@@ -1,16 +1,20 @@
 package edu.gmu.cs.infs740.music.Controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 
-	@RequestMapping(value = "/title", method = RequestMethod.POST, headers = {"Accept=text/xml, application/xml"})
-	public @ResponseBody  findItems(@RequestParam String param) {
-	}
+//	@RequestMapping(value = "/title", method = RequestMethod.POST, headers = {"Accept=text/xml, application/xml"})
+//	public @ResponseBody  findItems(@RequestParam String param) {
+//
+//	}
 }
