5902736585f18e0563888495c779e3515f703445
Wade Ward <wade.w.ward@gmail.com>
2013-11-05 01:29:30 -0500

    added existdb deps to pom

--- edu.gmu.cs.infs740.music/src/main/java/edu/gmu/cs/infs740/music/Controller/SearchController.java
+++ edu.gmu.cs.infs740.music/src/main/java/edu/gmu/cs/infs740/music/Controller/SearchController.java
@@ -8,11 +8,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.*;
+import org.xmldb.api.*;
+import javax.xml.transform.OutputKeys;
+import org.exist.xmldb.EXistResource;
+
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 
+//	Hey Reza, the existdb libs are loading properly now using the pom.
+//	Do Run As: maven:clean, then Run As: maven:install
 //	@RequestMapping(value = "/title", method = RequestMethod.POST, headers = {"Accept=text/xml, application/xml"})
 //	public @ResponseBody  findItems(@RequestParam String param) {
 //
