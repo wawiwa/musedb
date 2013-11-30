1f8451ce67ca63a3fb9199de7f2df44bcd01bcc0
Wade Ward <wade.w.ward@gmail.com>
2013-11-30 13:48:59 -0500

    sync

---
+++ edu.gmu.cs.infs740.music/src/main/java/org/musicalpatriots/service/businesslogic/DomainLogic.java
@@ -0,0 +1,44 @@
+package org.musicalpatriots.service.businesslogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQException;
+
+import net.xqj.exist.ExistXQDataSource;
+
+import org.musicalpatriots.xml.dao.CompositionDao;
+import org.musicalpatriots.xml.entity.CompositionEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class DomainLogic {
+	
+    public List<CompositionEntity> findByTitle (String title) {
+            try {
+                    XQDataSource xqs = new ExistXQDataSource();
+                    xqs.setProperty("serverName", "musicalpatriots.org");
+                    xqs.setProperty("port", "8899");
+                    xqs.setProperty("user", "admin");
+                    xqs.setProperty("password", "gmupatriots");
+                    XQConnection conn = xqs.getConnection();
+                    System.out.println("connection:" + conn);
+                    CompositionDao dao = new CompositionDao(conn);
+                    
+                    List<CompositionEntity> searchResult = dao.findByTitle(title);
+                    System.out.println("search result count:" + searchResult.size());
+                    System.out.println("search result title:" + searchResult.get(0).getMainTitle());
+                    
+                    return searchResult;
+            } catch (XQException ex) {
+                    ex.printStackTrace();
+            }                
+            return new ArrayList<CompositionEntity>();
+
+    }
+
+}
---
+++ edu.gmu.cs.infs740.music/src/main/java/org/musicalpatriots/xml/dao/CompositionDao.java
@@ -0,0 +1,54 @@
+package org.musicalpatriots.xml.dao;
+
+import java.util.List;
+
+import javax.xml.xquery.XQConnection;
+
+import org.musicalpatriots.xml.dao.AbstractDao;
+import org.musicalpatriots.xml.entity.CompositionEntity;
+import org.w3c.dom.Element;
+
+public class CompositionDao extends AbstractDao<CompositionEntity> {
+
+	public CompositionDao(XQConnection conn) {
+		super(conn);	
+	}
+
+	public CompositionEntity findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CompositionEntity> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void persist(CompositionEntity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<CompositionEntity> findByTitle(String title) {
+		String query = "for $composition in doc('/db/music/musicxml/file10.xml')/composition_collection/composition ";
+		query += "where contains($composition/title/main_title,\'" + title + "\')";
+		query += "return $composition";
+		
+		System.out.println("query: "+query);
+		List<CompositionEntity> result = search(query);
+		return result;
+	}
+
+	@Override
+	protected CompositionEntity readObject(Element element) {
+		CompositionEntity composition = new CompositionEntity();
+		composition.setMainTitle(element.getElementsByTagName("main_title").item(0).getTextContent());
+		composition.setWorkTitle(element.getElementsByTagName("work_title").item(0).getTextContent());
+		org.w3c.dom.Element publisherNode = (org.w3c.dom.Element)element.getElementsByTagName("publisher").item(0);
+		composition.setPublisher(publisherNode.getElementsByTagName("name").item(0).getTextContent());
+		return composition;
+	}
+
+}
